375700e1e2bb7eaac58dc6043e7bf2bc03775bd9
saltemose <sean@seanaltemose.com>
2020-08-18 16:52:33 -0400

    mobile styling

--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './main_page.css';
 import FrontPage1 from '../../assets/images/front_page1.png';
 import FrontPage2 from '../../assets/images/front_page2.png';
-import FrontMobile from '../../assets/images/front_mobile.png';
+import FrontMobile from '../../assets/images/mobile-front3.png';
 import FrontMobile1 from '../../assets/images/front_mobile1.png';
 
 class MainPage extends React.Component {
@@ -11,14 +11,20 @@ class MainPage extends React.Component {
     return (
       <div>
       <div className="desktop-front">
-        <img className="main-section-1" src={FrontPage1} alt=""></img>
-        <img className="main-section-2" src={FrontPage2} alt=""></img>
+      <div className="front-page-1">
+
+      </div>
         <footer>
         </footer>
       </div>
       <div className="mobile-front">
-      <img className="main-section-1" src={FrontMobile1} alt=""></img>
-       <br/> <img className="main-section-2" src={FrontMobile} alt=""></img>
+      <div className="front-page-1">
+      <div className="front-title">Stay in touch with <br/> Homeroom.</div>
+      </div>
+      <div className="front-title-b">
+        Connect with other students, participate in class discussions, and more.
+      </div>
+      <img className="front-mobile" src={FrontMobile}></img>
       </div>
       </div>
     );
